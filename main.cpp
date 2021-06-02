0bdc3f0212b18734dac5fddf3350dcbe91296914
Mark Kenneth Ferrer <81341766+Dev-Kenn@users.noreply.github.com>
2021-06-02 12:58:56 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	
+	int distance = 5400;
+	int speed = 220;
+	int time;
+	
+	time = 5400/220;
+	cout <<"distance = 5400 miles\nspeed = 220 mph\n\n";
+	cout <<"The estimated hours to arrive is: " <<time;
+	
+	return 0;
+}
